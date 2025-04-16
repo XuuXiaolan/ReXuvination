b43fe624d4403a33967a1c2669bee790521d51c3
XuuXiao <ammaroaladel@Gmail.com>
2025-04-16 20:17:35 +0100

    added a preloader patch to add Awake to PlayerPhysicsRegion

--- Plugin/src/Patches/PlayerPhysicsRegionPatch.cs
+++ Plugin/src/Patches/PlayerPhysicsRegionPatch.cs
@@ -1,9 +1,12 @@
-/*using System;
 using HarmonyLib;
-using UnityEngine;
 
 namespace ReXuvination.src.Patches;
 [HarmonyPatch(typeof(PlayerPhysicsRegion))]
 static class PlayerPhysicsRegionPatch
 {
-}*/
+    [HarmonyPatch("Awake"), HarmonyPostfix]
+    public static void PlayerPhysicsRegionAwakePatch(PlayerPhysicsRegion __instance)
+    {
+        ReXuvination.Log.LogInfo($"Patched PlayerPhysicsRegion.Awake in Object: {__instance.GetInstanceID()}");
+    }
+}
--- Preloader/src/ReXuvination.cs
+++ Preloader/src/ReXuvination.cs
@@ -5,85 +5,81 @@ using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using Mono.Cecil;
-using FieldAttributes = Mono.Cecil.FieldAttributes;
 
-namespace ReXuvination.Preloader
+namespace ReXuvination.Preloader.src;
+internal class ReXuvination
 {
-    internal class ReXuvination
-    {
-       
-        public const string GUID = MyPluginInfo.PLUGIN_GUID;
-        public const string NAME = MyPluginInfo.PLUGIN_NAME;
-        public const string VERSION = MyPluginInfo.PLUGIN_VERSION;
-		
-		internal static readonly BepInPlugin Plugin = new BepInPlugin(GUID, NAME, VERSION);
-		
-        internal static ManualLogSource Log { get; } = Logger.CreateLogSource(nameof(ReXuvination));
-        
-        public static IEnumerable<string> TargetDLLs { get; } = new string[] { "Assembly-CSharp.dll" };
+    
+    public const string GUID = MyPluginInfo.PLUGIN_GUID;
+    public const string NAME = MyPluginInfo.PLUGIN_NAME;
+    public const string VERSION = MyPluginInfo.PLUGIN_VERSION;
+    
+    internal static readonly BepInPlugin Plugin = new BepInPlugin(GUID, NAME, VERSION);
+    
+    internal static ManualLogSource Log { get; } = Logger.CreateLogSource(nameof(ReXuvination));
+    
+    public static IEnumerable<string> TargetDLLs { get; } = new string[] { "Assembly-CSharp.dll" };
 
-        private static readonly string MainDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        
-        public static void Patch(AssemblyDefinition assembly)
+    private static readonly string MainDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    
+    public static void Patch(AssemblyDefinition assembly)
+    {
+        Log.LogWarning($"Patching {assembly.Name.Name}");
+        if (assembly.Name.Name == "Assembly-CSharp")
         {
-            Log.LogWarning($"Patching {assembly.Name.Name}");
-            if (assembly.Name.Name == "Assembly-CSharp")
+            foreach (TypeDefinition type in assembly.MainModule.Types)
             {
-                foreach (TypeDefinition type in assembly.MainModule.Types)
+                if (type.FullName == "PlayerPhysicsRegion")
                 {
-                    if (type.FullName == "GrabbableObject")
-                    {
-                        //type.Fields.Add(new FieldDefinition("AdditionalNetworking_isInitialized", FieldAttributes.Private, type.Module.ImportReference(typeof(bool))));
-                        //Log.LogInfo($"Adding field 'AdditionalNetworking_isInitialized' to {type.FullName}");
-                    }
+                    type.AddMethod("Awake", Mono.Cecil.MethodAttributes.Public);
                 }
             }
-			
-            if (!PluginConfig.Enabled.Value) 
-                return;
-            
-            var outputAssembly = $"{PluginConfig.OutputPath.Value}/{assembly.Name.Name}{PluginConfig.OutputExtension.Value}";
-            Log.LogWarning($"Saving modified Assembly to {outputAssembly}");
-            assembly.Write(outputAssembly);
         }
         
-        // Cannot be renamed, method name is important
-        public static void Initialize()
-        {
-            Log.LogInfo($"Prepatcher Started");
-            PluginConfig.Init();
-        }
-
-        // Cannot be renamed, method name is important
-        public static void Finish()
-        {
-            Log.LogInfo($"Prepatcher Finished");
-        }
-		
-		public static class PluginConfig
-        {
-            public static void Init()
-            {
-                var config = new ConfigFile(Utility.CombinePaths(MainDir, "Development.cfg"), true, Plugin);
-                //Initialize Configs
-                Enabled = config.Bind("DevelOptions", "Enabled", false, "Enable development dll output");
-                OutputPath = config.Bind("DevelOptions", "OutputPath", MainDir, "Folder where to write the modified dlls");
-                OutputExtension = config.Bind("DevelOptions", "OutputExtension", ".pdll", "Extension to use for the modified dlls\n( Do not use .dll if outputting inside the BepInEx folders )");
-
-                //remove unused options
-                PropertyInfo orphanedEntriesProp = config.GetType()
-                    .GetProperty("OrphanedEntries", BindingFlags.NonPublic | BindingFlags.Instance);
-
-                var orphanedEntries = (Dictionary<ConfigDefinition, string>)orphanedEntriesProp!.GetValue(config, null);
-
-                orphanedEntries.Clear(); // Clear orphaned entries (Unbinded/Abandoned entries)
-                config.Save(); // Save the config file
-            }
-
-            internal static ConfigEntry<bool> Enabled;
-            internal static ConfigEntry<string> OutputPath;
-            internal static ConfigEntry<string> OutputExtension;
-        }
-
+        if (!PluginConfig.Enabled.Value) 
+            return;
+        
+        var outputAssembly = $"{PluginConfig.OutputPath.Value}/{assembly.Name.Name}{PluginConfig.OutputExtension.Value}";
+        Log.LogWarning($"Saving modified Assembly to {outputAssembly}");
+        assembly.Write(outputAssembly);
     }
+    
+    // Cannot be renamed, method name is important
+    public static void Initialize()
+    {
+        Log.LogInfo($"Prepatcher Started");
+        PluginConfig.Init();
+    }
+
+    // Cannot be renamed, method name is important
+    public static void Finish()
+    {
+        Log.LogInfo($"Prepatcher Finished");
+    }
+    
+    public static class PluginConfig
+    {
+        public static void Init()
+        {
+            var config = new ConfigFile(Utility.CombinePaths(MainDir, "Development.cfg"), true, Plugin);
+            //Initialize Configs
+            Enabled = config.Bind("DevelOptions", "Enabled", false, "Enable development dll output");
+            OutputPath = config.Bind("DevelOptions", "OutputPath", MainDir, "Folder where to write the modified dlls");
+            OutputExtension = config.Bind("DevelOptions", "OutputExtension", ".pdll", "Extension to use for the modified dlls\n( Do not use .dll if outputting inside the BepInEx folders )");
+
+            //remove unused options
+            PropertyInfo orphanedEntriesProp = config.GetType()
+                .GetProperty("OrphanedEntries", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var orphanedEntries = (Dictionary<ConfigDefinition, string>)orphanedEntriesProp!.GetValue(config, null);
+
+            orphanedEntries.Clear(); // Clear orphaned entries (Unbinded/Abandoned entries)
+            config.Save(); // Save the config file
+        }
+
+        internal static ConfigEntry<bool> Enabled;
+        internal static ConfigEntry<string> OutputPath;
+        internal static ConfigEntry<string> OutputExtension;
+    }
+
 }
